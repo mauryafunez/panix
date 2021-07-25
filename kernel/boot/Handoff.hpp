589df85bf2e149777071ec7f1fd37eafcfeb87d6
Keeton T. Feavel <keetonfeavel@gmail.com>
2021-07-25 19:02:00 -0400

    Rename graphics classes, start printing refactor Ran `clang-tidy` on actively worked on files

--- kernel/boot/Handoff.hpp
+++ kernel/boot/Handoff.hpp
@@ -97,7 +97,7 @@ public:
     // Getters
     const char* getCmdLine()                    { return _cmdline; }
     const void* getHandle()                     { return _handle; }
-    graphics::FramebufferInfo getFramebufferInfo()    { return _fbInfo; }
+    graphics::Framebuffer getFramebufferInfo()  { return _framebuffer; }
     HandoffBootloaderType getBootType()         { return _bootType; }
 
 private:
@@ -107,7 +107,7 @@ private:
     void* _handle;
     char* _cmdline;
     uint32_t _magic;
-    graphics::FramebufferInfo _fbInfo;
+    graphics::Framebuffer _framebuffer;
     HandoffBootloaderType _bootType;
 };
 
